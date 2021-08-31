d209e01edb47d1aae6656a71e1db916ff9902867
jeremy <wangjeremy2000@gmail.com>
2021-08-31 16:24:09 -0400

    new repo for f21

--- zombielab/driver/tests/find-survivors.sol
+++
@@ -1,5 +0,0 @@
-abandoned-town/dark-basement:BSAVEME!!!AAAAAAAAAAAAAAAAAAAAINS
-abandoned-town/gas-station:SAVEASQLWRJWQWQORIAHSKRQWLRJQWORHME!!!!
-abandoned-town/greenhouse:BRAAASAVE      ME!AAAAAAAAAAAAINS
-abandoned-town/scary-attic:SAVE US, OR JUST SAVE ME!!!!!!!!!!!
-abandoned-town/small-shack:SAVE US! WE'RE IN HERE! HELP ME!
