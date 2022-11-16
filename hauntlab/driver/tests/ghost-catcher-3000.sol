df0686da9514527bf1698e8d2e61542573fe47bf
Laura Yao <lyao@lauras-air-3.wifi.local.cmu.edu>
2022-11-16 01:09:24 -0500

    release hauntlab & zombielab

---
+++ hauntlab/driver/tests/ghost-catcher-3000.sol
@@ -0,0 +1,5 @@
+library/A Child Called 'It' - history
+library/How the Grinch Stole Christmas! - action
+library/The Count of Monte Cristo - magic
+library/The Hunchback of Notre-Dame - romance
+library/Torment - action
---
+++ hauntlab/driver/tests/ghost-catcher-3001.sol
@@ -0,0 +1,9 @@
+library/A Child Called 'It' - history
+library/A Great and Terrible Beauty - magic
+library/Harry Potter and the Order of the Phoenix - scifi
+library/How the Grinch Stole Christmas! - action
+library/Pippi Longstocking - history
+library/The Count of Monte Cristo - magic
+library/The Hunchback of Notre-Dame - romance
+library/The Very Hungry Caterpillar - history
+library/Torment - action
