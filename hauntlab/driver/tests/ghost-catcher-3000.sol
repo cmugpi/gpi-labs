6b092bed7a1b412610d2cbe5992cbb7dbc5e90db
Tianyue Sun <tianyuerubysun@gmail.com>
2025-09-05 15:48:03 -0400

    Reset: Semester 2025 Fall

--- hauntlab/driver/tests/ghost-catcher-3000.sol
+++
@@ -1,5 +0,0 @@
-library/A Child Called 'It' - history
-library/How the Grinch Stole Christmas! - action
-library/The Count of Monte Cristo - magic
-library/The Hunchback of Notre-Dame - romance
-library/Torment - action
